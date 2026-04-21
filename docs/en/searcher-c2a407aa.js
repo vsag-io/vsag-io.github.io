30c143c9b46149b031ce52a1705fb16204ea0bb2
wxyucs <wxyucs@gmail.com>
2026-04-21 08:59:10 +0000

    Deploying to gh-pages from @ vsag-io/vsag-io.github.io@6858fbcbe4df8f5c92440a6be7e7073427742758 🚀

--- docs/en/searcher-c2a407aa.js
+++ docs/en/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-07d75732.js',
+                path_to_root + 'searchindex-a23063df.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
